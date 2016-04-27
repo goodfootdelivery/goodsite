43f1e35727ca83a9e62260077bf523fe8c3d8646
Connor Sullivan <sully4792@gmail.com>
2016-04-27 13:17:02 -0400

    DATABASES setting tweaked

---
+++ goodfoot/apps.py
@@ -0,0 +1,9 @@
+from importlib import import_module
+from django.apps import AppConfig as BaseAppConfig
+
+
+class AppConfig(BaseAppConfig):
+    name = "goodfoot"
+
+    def ready(self):
+        pass
--- goodfoot/settings.py
+++ goodfoot/settings.py
@@ -42,8 +42,9 @@ elif socket.gethostname() == 'Connors-MacBook-Pro.local':
 
 else:
     import dj_database_url
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=500)
+    }
     DEBUG = True
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
